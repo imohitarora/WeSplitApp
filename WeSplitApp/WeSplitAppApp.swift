62108cfcf18dd2ae6ced317a9d48e41a4f193a91
Mohit Arora <me@aboutmohit.com>
2024-04-18 15:48:50 -0400

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Mohit Arora on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
